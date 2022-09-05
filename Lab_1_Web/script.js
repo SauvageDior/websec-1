321db8d40d765add11cebf902dcb8ea5cae34bcb
SauvageDior <92827447+SauvageDior@users.noreply.github.com>
2022-09-05 14:40:25 +0300

    updated js code

--- Lab_1_Web/script.js
+++ Lab_1_Web/script.js
@@ -5,8 +5,17 @@ function buttonClickHandler() {
     console.log(selecter.value);
     
     let value1 = document.getElementById("input1").value;
+    //if (isNaN(value1)) {alert('введите число 1')}
     let value2 = document.getElementById("input2").value;
+    //if (isNaN(value2)) {alert('введите число 2')}
     let res;
+
+    if ((isNaN(value1))||(isNaN(value2))) {
+        alert('число нада(((((');
+        document.getElementById("result").innerText = "не числа(";
+    }
+    else {
+
     if (selecter.value == "plus") {
      res = parseFloat(value1) + parseFloat(value2);
     }
@@ -22,6 +31,7 @@ function buttonClickHandler() {
        }
     
     document.getElementById("result").innerText = res;
+    }
     
 
     console.log(res);
