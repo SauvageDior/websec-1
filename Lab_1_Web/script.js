291b2c4ae1879ccb934deee9699e9a0cd98b491a
SauvageDior <92827447+SauvageDior@users.noreply.github.com>
2022-09-11 00:57:28 +0300

    накопление строк, исправление ошибок
    случайно не туда куда нужно файл закинул в прошлый раз и все продублировалось. Пришлось удалять, если вдруг странно выглядеть история комитов будет.

--- Lab_1_Web/script.js
+++ Lab_1_Web/script.js
@@ -9,29 +9,40 @@ function buttonClickHandler() {
     let value2 = document.getElementById("input2").value;
     //if (isNaN(value2)) {alert('введите число 2')}
     let res;
+    let total = "";
+    let x;
 
-    if ((isNaN(value1))||(isNaN(value2))) {
+    if ((isNaN(value1))||(isNaN(value2))||(!value1)||(!value2)) {
         alert('число нада(((((');
         document.getElementById("result").innerText = "не числа(";
     }
     else {
 
-    if (selecter.value == "plus") {
+    if (selecter.value === "plus") {
      res = parseFloat(value1) + parseFloat(value2);
     }
-    if (selecter.value == "minus") {
+    if (selecter.value === "minus") {
      res = parseFloat(value1) - parseFloat(value2);
     }
-    if (selecter.value == "mul") {
+    if (selecter.value === "mul") {
         res = parseFloat(value1) * parseFloat(value2);
        }
-    if ((selecter.value == "div")&&(parseFloat(value2)!=0)) {
-        
+    if ((selecter.value === "div")&&(Math.abs(parseFloat(value2)) > Number.EPSILON)) {
         res = parseFloat(value1) / parseFloat(value2);
        }
-    
-    document.getElementById("result").innerText = res;
+    else if ((selecter.value === "div")&&(Math.abs(parseFloat(value2)) < Number.EPSILON))
+    {
+        res = "ответа нет(";
     }
+    
+    
+    document.getElementById("result").value  +=   value1 +" "+ selecter.value +" "+ value2 + " = " + res + '\n'; 
+    
+    
+
+    }
+    
+    
     
 
     console.log(res);
